b1428e7636f0d811f7a2c69c9c63a73f6c061c1b
ungoodman <suchanej.19@gmail.com>
2023-01-30 23:57:58 +0700

    init

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,15 @@
 
 // Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
 // See the Device Info tab, or Template settings
-#define BLYNK_TEMPLATE_ID           "TMPLVDbaUwQu"
-#define BLYNK_DEVICE_NAME           "Quickstart Device"
-#define BLYNK_AUTH_TOKEN            "-_bTnrMzb5-a9TtHmSMpsc303ePpHzvq"
+#define BLYNK_TEMPLATE_ID "TMPLVDbaUwQu"
+#define BLYNK_DEVICE_NAME "Quickstart Template"
+#define BLYNK_AUTH_TOKEN            "cLG_aZR-qL6SPuL35mH00PmM9Mq2MnCN"
 
 
 // Comment this out to disable prints and save space
 #define BLYNK_PRINT Serial
 
-#include<Arduino.h>
+
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
